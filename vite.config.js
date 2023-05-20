14486b9730659b5e47d7a9b9faeb8d18bc72b744
DoWhileNotDone <dowhilenotdone@hotmail.com>
2023-05-20 11:56:37 +0100

    tmp

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,11 @@ export default defineConfig({
             },
         }),
     ],
+    // server: {
+    //     host: true,
+    //     port: 5173,
+    //     watch: {
+    //         usePolling: true,
+    //     },
+    // },
 });
